22eaafb8274ee8acb5e46b66d51c1f16f6a010cd
Leo Dadi <leo.dadi@student-cs.fr>
2024-10-16 09:43:56 +0200

    Deno path changed

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { serve } from "<https://deno.land/std@0.119.0/http/server.ts>";
+import { Deno } from "deno";
 
 async function handler(_req: Request): Promise<Response> {
   return new Response("Hello World");
 }
 
-serve(handler);
+Deno.serve(handler);
